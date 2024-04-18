7dfe7b589b290c96bafdc8d4b2157a3969df114f
Omer Faruk Buyukbas <68295684+omerbykbs@users.noreply.github.com>
2024-04-18 16:46:48 +0200

    Update math_tool_for_agent.py

--- Agent Tools/math_tool_for_agent.py
+++ Agent Tools/math_tool_for_agent.py
@@ -1,14 +1,9 @@
 from langchain.agents import AgentType, Tool, initialize_agent
-# pip install langchain-openai
 from langchain_openai.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-
-#  In order to set this Bing search, go to
-# https://www.microsoft.com/en-us/bing/apis/bing-web-search-api
-# and click "Try now" to get a Bing web search API key
-# put your API key in the env variable BING_SUBSCRIPTION_KEY
+# For Web Search Tool (Note: Bing Search API key is required to be set in env as BING_SUBSCRIPTION_KEY)
 from langchain.utilities.bing_search import BingSearchAPIWrapper
-
+# For Math Calculator Tool
 from langchain.chains import LLMMathChain
 
 import os
@@ -49,8 +44,9 @@ pydict = agent.invoke({"input": "Hi, I am Bob"})
 print(pydict["output"])
 pydict = agent.invoke({"input": "What's my name?"})
 print(pydict["output"])
+# Web searching
 pydict = agent.invoke({"input": "Who is the CEO of LinkedIn in 2023?"})
 print(pydict["output"])
-
-pydict = agent.invoke({"input": "What is 2*pi*10^2"})
+# Math calculating
+pydict = agent.invoke({"input": "What is the square root of 840889"})
 print(pydict["output"])
